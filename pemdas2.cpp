159f2de38c15751cb0c0b005b0dd0db8b02a8f07
Diemaz Dawara <71909895+JamieVardyDD@users.noreply.github.com>
2026-02-24 20:05:38 +0700

    add pemdas2

---
+++ pemdas2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int p, l, luas;
+
+    //langkah 2
+    cout << "Masukan Pnajang =";
+    cin >> p;
+
+    //langkah 3
+    cout << "Masukan lebar =";
+    cin >> l;
+
+    //langkah 4
+    luas = p * 1;
+
+    //langkah 5
+    cout << "Hasilnya =" << luas << endl;
+    cout << "terima kasih";
+}
